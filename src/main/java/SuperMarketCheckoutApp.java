d668e187336cac2cb49faace0483cdc225e830e2
smosgr <stel.moschos@gmail.com>
2018-05-13 22:20:44 +0100

    first commit

---
+++ src/main/java/SuperMarketCheckoutApp.java
@@ -0,0 +1,10 @@
+public class SuperMarketCheckoutApp {
+
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static String init(){
+        return "Hi";
+    }
+}
---
+++ src/test/java/SuperMarketCheckoutAppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class SuperMarketCheckoutAppTest {
+
+    @Test
+    public void testHi(){
+        SuperMarketCheckoutApp checkoutApp = new SuperMarketCheckoutApp();
+        String s = checkoutApp.init();
+
+        assertEquals("Hi", s);
+        }
+}
