e342768ba848293fe8c3e880b31adb7ab05063a7
smosgr <stel.moschos@gmail.com>
2018-05-20 21:31:08 +0100

    removed console class. No console UI is needed

--- src/main/java/gr/smos/SuperMarketCheckoutApp.java
+++ src/main/java/gr/smos/SuperMarketCheckoutApp.java
@@ -8,14 +8,5 @@ public class SuperMarketCheckoutApp {
 
     public static void init() {
 
-        ConsoleApp consoleApp = new ConsoleApp();
-        consoleApp.setupConsole();
-
-
-        while (consoleApp.readFromConsole().equals("Q")) {
-            consoleApp.readFromConsole();
-        }
-
-        System.exit(1);
     }
 }
